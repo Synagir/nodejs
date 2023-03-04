888f60ad5f92a28ca9ef0ad07def160d1c4b9bdf
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-05 01:04:30 +0900

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import style from './index.module.scss';
 const Index = () => {
   return (
     <>
-      <p className={style.text}>Hello World!</p>
+      <p className={style.text}>Hello World! I am here</p>
     </>
   );
 };
