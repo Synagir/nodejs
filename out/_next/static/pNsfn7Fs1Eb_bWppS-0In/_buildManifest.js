487269b532e7af631fbc2dfcdb2dd336b23349dc
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-06 01:33:45 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/pNsfn7Fs1Eb_bWppS-0In/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab23f3e52584ae07.css","static/chunks/pages/index-e6688dc7b0834a00.js"],"/_error":["static/chunks/pages/_error-19e73c3be68140bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
