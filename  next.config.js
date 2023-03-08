a8dda2bd866bc77568c66b211759f31cbebf60da
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-08 14:42:51 +0900

    update next.config.js

---  next.config.js
+++  next.config.js
@@ -1,15 +1,5 @@
-// const isProd = process.env.NODE_ENV === 'production';
-// const url = 'https://synagir.github.io/nodejs';
+const url = 'https://synagir.github.io/nodejs';
 
-// module.exports = {
-//   assetPrefix: isProd ? url : undefined,
-// };
-
-const nextConfig = {
-  reactStrictMode: true,
-
-  // Add basePath
-  basePath: 'https://synagir.github.io/nodejs',
+module.exports = {
+  assetPrefix: url,
 };
-
-module.exports = nextConfig;
