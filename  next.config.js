647192973c27c6e9636a1d0b61ec416b71b21050
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-12 01:24:11 +0900

    update

---  next.config.js
+++  next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const debug = process.env.NODE_ENV !== 'production';
+const repository = 'nodejs';
+
+const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  assetPrefix: '/pages',
+  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
+
+module.exports = nextConfig;
---
+++ config/config.js
@@ -0,0 +1,3 @@
+const url = 'https://synagir.github.io/nodejs';
+
+export const prefix = process.env.NODE_ENV === 'production' ? url : '';
