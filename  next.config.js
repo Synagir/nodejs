b2d865fa61a620073c8d1fa09a5c1c8d7a2650b4
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-12 02:05:46 +0900

    update

---  next.config.js
+++  next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== 'production';
-const repository = 'nodejs';
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
