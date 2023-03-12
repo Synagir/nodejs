c8849ce4b9fd0eb605ec88ba7532cd71baff295f
Synagir <47382819+Synagir@users.noreply.github.com>
2023-03-12 04:35:16 +0000

    Deploying to gh-pages from  @ 65315bc96534d9bbeb10126e577ad830d0dfb69b 🚀

---
+++ _next/static/5J6qs4ld4wAlpKMdS4auA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab23f3e52584ae07.css","static/chunks/pages/index-b9b7d059b33f992b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
