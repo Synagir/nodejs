363a885e66ae5b96ce1df3384c2cd047e259f9bb
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-12 13:07:47 +0900

    update

---  next.config.js
+++  next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  basePath: '/pages',
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://synagir.github.io/nodejs' : undefined,
 };
